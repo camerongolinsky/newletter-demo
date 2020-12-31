370f0db2b0a832e603fdbc9a2b888189a20f71d1
Cameron Golinsky <camerongolinsky@gmail.com>
2020-12-31 01:43:05 -0800

    maybe first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const request = require('request');
+const https = require('https');
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended:true})) 
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res){
+    res.sendFile(__dirname + '/signup.html')
+})
+
+app.post('/', function(req, res){
+
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email
+
+    const data = {
+        members : [
+            {
+                email_address: '',
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const options = {
+        method: 'POST',
+        auth: 'CameronG: fd0ff3960b44a80b33789ba9c3574bff-us7'
+    }
+
+    const url = 'https://us7.api.mailchimp.com/3.0/lists/739e3a21de'
+
+    const request = https.request(url, options, function(response){
+
+        if (response.statusCode === 200){
+            res.sendFile(__dirname + '/success.html')
+        } else {
+            res.sendFile(__dirname + '/failure.html')
+        }
+
+        response.on('data', function(data){
+            console.log(JSON.parse(data))
+        })
+    })
+
+
+    request.write(jsonData);
+    request.end();
+})
+
+app.post('/failure', function(req, res){
+    res.redirect('/')
+})
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log(`Server is running on Port 3000`)
+})
+
